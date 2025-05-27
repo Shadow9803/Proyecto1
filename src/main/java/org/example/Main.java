2202b080cbcf342955417ae8d773dd693de4dbce
Shadow9803 <dayanamh1998@gmail.com>
2025-05-27 11:28:31 -0500

    fit: poroyecteo 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,30 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+                String nombre1= "Diego Alejandro Osorno Cardona";
+                String nombre2= "Jose Jesus Vargas";
+                String nombre3= "Julian Santiago Serna Villada";
+                String nombre4= "Jacobo Gonzales Villegas";
+                String nombre5= "Andrea Insignares";
+                String nombre6= "Javier Martines";
+                int edad1= 26;
+                int edad2= 23;
+                int edad3= 25;
+                int edad4= 24;
+                int edad5= 26;
+                int edad6= 22;
+                int cedula1= 1214742659;
+                int cedula2= 1402568783;
+                int cedula3= 1221475635;
+                int cedula4= 1035648759;
+                int cedula5= 1452678962;
+                int cedula6= 1643655684;
+                String barrio1= "Bello Oriente";
+                String barrio2= "Manrique";
+                String barrio3= "Buenos aires";
+                String barrio4= "Santa Elena";
+                String barrio5= "Aranjuez";
+                String barrio6= "Santo Domingo";
+
     }
 }
